b7b99e301187dffb91ff95a65d71bfdd09abbc99
Yeonji Lee <yeonji.dev@gmail.com>
2021-04-10 20:55:08 +0900

    code : yeonji boj 1541 greedy 잃어버린 괄호

---
+++ greedy/1541_잃어버린 괄호/연지/sol1.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open("input.txt")
+
+
+input_lst = input().split('-')
+print(input_lst)
+tmp = input_lst[0].split('+')
+res = 0
+for ele in tmp:
+    res += int(ele)
+
+for i in range(1, len(input_lst)):
+    tmp = input_lst[i].split('+')
+    for ele in tmp:
+        res -= int(ele)
+
+print(res)
+
